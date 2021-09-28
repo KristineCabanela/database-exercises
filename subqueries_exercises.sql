00d7ec66a6cd4217a37d1f5c8723c6e17f2fb79b
kristine cabanela <kristinecabanela@gmail.com>
2021-09-28 16:00:59 -0500

    commit Tuesday pm exercises

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -13,6 +13,8 @@ WHERE hire_date IN
 FROM employees
 WHERE emp_no = 101010);
 
+
+
 -- 2. Find all the titles ever held by all current employees with the first name Aamod.
 
 SELECT title
@@ -33,9 +35,10 @@ WHERE to_date > CURDATE()
 );
 
 
+
 -- 4.  Find all the current department managers that are female. List their names in a comment in your code.
 
-SELECT first_name, last_name
+SELECT *
 FROM employees
 WHERE gender = 'F'
 AND emp_no IN(
@@ -48,7 +51,39 @@ WHERE to_date > CURDATE()
 -- 5. Find all the employees who currently have a higher salary than the companies overall, historical average salary.
 
 
+SELECT *
+FROM employees
+WHERE 
+SELECT emp_no
+FROM salaries
+WHERE salary >
+FROM employees 
+	(SELECT AVG(salary)
+	FROM salaries)
+AND to_date > currdate();
 
 -- 6. How many current salaries are within 1 standard deviation of the current highest salary? (Hint: you can use a built in function to calculate the standard deviation.) What percentage of all salaries is this?
 
+-- MAX SALARY
+select max(salary) from salaries where to_date > now();
 
+
+-- what is the 1 std for current salary?
+
+select std(salary) from salaries where to_date > now();
+
+
+select count(salary)
+from salaries
+where to_date > now()
+and salary > (
+(select max(salary) from salaries where to_date > now())
+- (select std(salary) from salaries where to_date > now ())
+);
+
+
+SELECT salary, 
+    (salary - (SELECT AVG(salary) FROM salaries)) 
+    / 
+    (SELECT stddev(salary) FROM salaries) AS zscore
+FROM salaries;
--- temporary_tables.sql
+++ temporary_tables.sql
@@ -67,9 +67,68 @@ SELECT * FROM payments_from_sakila;
 UPDATE payments_from_sakila
 SET amount_in_cents = (amount * 100);
 
-
 SELECT * FROM payments_from_sakila;
 
 
 -- 3. Find out how the current average pay in each department compares to the overall, historical average pay. In order to make the comparison easier, you should use the Z-score for salaries. In terms of salary, what is the best department right now to work for? The worst? */
+
+
+-- step 1 find historic average pay
+
+use employees;
+select avg(salary) from salaries;
+
+-- 63810
+
+-- step 2: find historic standard deviation
+
+select stddev(salary) from salaries;
+
+-- 16904
+
+
+-- step 3: find current average pay in each department
+
+select dept_name, avg(salary) as current_dept_salaries
+from departments
+join dept_emp using (dept_no)
+join salaries using (emp_no)
+group by dept_name;
+
+
+
+
+CREATE TEMPORARY TABLE hopper_1559.salaries_by_dept
+select dept_name, avg(salary) as current_dept_salaries
+from departments
+join dept_emp using (dept_no)
+join salaries using (emp_no)
+group by dept_name;
+
+
+SELECT * FROM salaries_by_dept;
+
+
+
+CREATE TEMPORARY TABLE hopper_1559.salaries_by_dept
+SELECT dept_name, 
+((AVG(salary) - (SELECT AVG(salary) 
+FROM employees.salaries)) /
+(SELECT std(salary)
+FROM employees.salaries)) AS z_score
+FROM employees.salaries
+JOIN employees.dept_emp USING(emp_no)
+JOIN employees.departments USING(dept_no)
+GROUP BY dept_name;
+
+drop table salaries_by_dept;
+SELECT * FROM salaries_by_dept;
+
+
+
+
+
+
+
+
 
